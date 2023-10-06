556967464b66d2a2cd419c1f85ccf9e6df94e5c6
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-06 08:15:07 +0000

    datatype

---
+++ 01js/data_types.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js as a new version
+ //alter(3+3) // we are using nodejs, in browser
+
+ console.log(3 = 3);
+
+
+ console.log("Utkarsh");
+
+
+
+ let name = "Utkarsh"   //string// 
+ let age = 23   //number . number=>2 to power 53 //
+ let isloggedin = false //boolean//
+
+//null// standlone value and ptint as a  object
+let sate;// Undefined//
+//symbol =>for unique//
+
+//object//
+
+console.log(typeof null); // we get object//
+console.log(typeof undefined); // we get undefined//
+
+
