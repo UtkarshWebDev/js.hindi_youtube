a0b85f1e3d9eba49b8f1d76d30753cad550e2514
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-06 18:22:45 +0000

    operation

---
+++ 01js/operation.js
@@ -0,0 +1,46 @@
+let srt1 = "hello"
+let str2 = " Utkarsh"
+
+let str3 = srt1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 +"2");
+
+
+console.log((3 + 4) * 5 % 6); // we dont have to use this practically 
+
+console.log(+true); // we get 1
+console.log(+""); // we dont have to use this coz this is not the proper way , but if we put the value (+"") we get 0
+
+let num1, num2, num3
+
+ num1 = num2 = num3 = 2 + 2 // dont use this for alll the time 
+   
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter); // we get 101 
+
+ let gamePointer = 100
+++gamePointer;
+ console.log(gamePointer); // we get 101
+
+
+ // link to study 
+
+ // https://tc39.es/ecma262/mutipage/abstract-operations.html#sec-type-conversion
+ 
+
+
+
+
+
+ 
+
+
+
+
+ 
