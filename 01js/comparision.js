dd4b9f6e87d22ccd834b6d3e6a25da948db21993
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-07 11:13:04 +0000

    comparision

---
+++ 01js/comparision.js
@@ -0,0 +1,36 @@
+//console.log(2 > 0);             // true
+//console.log(2 >= 0);             //true
+//console.log(2 < 1);            //false
+//console.log(2 == 1);        //false
+//console.log(2 != 1);       //true
+
+
+
+//console.log("2" = 1); // true
+//console.log("02" != 1); //true
+
+
+/*the reason is that an equatliy cheack == and comparisons > <  >= <= works differently.
+comparisons convert nullto a number , treating it as 0.
+that's why (3) null  >= is true and (1) null >0 is false.*/
+
+
+
+/*console.log( null > 0);   // false
+console.log( null == 0);  //false
+console.log( null >= 0); // true
+
+
+console.log(undefined == 0); //false
+console.log(undefined > 0);   // false
+console.log(undefined < 0);  //false                            this all type of conversion put us in confusion */
+
+
+//===
+  console.log( "2" === 2);  // false
+
+
+
+  
+
+
