a6c02d654c0fcabfa0a88943673bc8f7602ec349
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-07 12:29:24 +0000

    datatypes and memory _summary

--- 01js/data_type_summary.js
+++ 01js/data_type_summary.js
@@ -42,3 +42,33 @@ const myfunction = function(){
 console.log( typeof myfunction );
 
 
+/******************************memory********************************* */
+
+//their are two type of memory
+
+//1 stack (primitive) // it will do copy the values
+
+
+let myNameis = "Utkarsh"
+  
+let anotherName = myNameis
+anotherName = "bittu"
+
+console.log(myNameis);
+console.log(anotherName);
+
+
+
+//2 Heap (non primitive)   // it wil take refrance
+
+
+let userOne = {
+    email: "utkarshprajapati6@gmail.com",
+    upi : "9860997855@paytm"
+}
+
+let userTwo = userOne
+userTwo.email = "satyamprajapati6@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
