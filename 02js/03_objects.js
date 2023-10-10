52b72e58460f499b094e11fe56b73d5a8d63173c
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-10 10:30:42 +0000

    1_object

---
+++ 02js/03_objects.js
@@ -0,0 +1,50 @@
+
+
+
+//object literals
+const mySym = Symbol("mykey1") // symbol
+
+
+
+const JsUser = {
+    name: "Utkarsh",
+    "fullName":"Utkarsh Prajapati",
+     [mySym]:"mykey1", //symbol
+    age: 23,
+    location: "Nagpur",
+    email: "utkarshprajapati6@gmail.com",
+    isLoggedIn:false,
+    lastLoggedIn:["monday", "saturday"]
+
+
+}
+
+
+//console.log(JsUser.email); // not this coz name is work as a string 
+//console.log(JsUser["email"]);  //use this 
+//console.log(JsUser["fullName"]); 
+//console.log(JsUser[mySym]) //symbol
+
+
+
+JsUser.email = "satyamprajapati6@gmail.com" // to change something
+//Object.freeze(JsUser) //hear it will freeze ,it will not change after freeze
+JsUser.email = "utkasrh9860997855@gmail.com" // not change cuz it us freeze
+
+
+//console.log(JsUser);
+
+///////////////function///////////////////////////////
+
+
+JsUser.greeting = function(){
+    console.log("hello js user");
+}
+
+
+JsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.fullName}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
