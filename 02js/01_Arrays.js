dfda1b9b6a54bb6c9a9e3536cbacfd8ec78b15cf
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-10 07:40:32 +0000

    Arrays

---
+++ 02js/01_Arrays.js
@@ -0,0 +1,47 @@
+//array
+
+ //const myArr = [0 , 1 , 2 , 3 , 4 , 5]
+ const myHeros = ["spiderman , superman"]
+
+ //console.log(myArr[1]);
+    
+ ///////>>>>>>>Arrays method/////////////
+
+ myArr.push(6) /// add number 6 
+ myArr.pop()  // cut no 
+  
+ myArr.unshift(9)  // add hota hai arrays ke 1st no m
+ myArr.shift()    // yeh hata deta h samne se 
+    
+
+ //console.log(myArr);
+
+//console.log(myArr.includes(9)); // includes tells us thst 9 is their or not in boolean 
+
+
+//console.log(myArr.indexOf(9)); // indexof tells us that the value is their or not if it or if not it will give -1
+
+
+const newArr = myArr.join()
+
+//console.log(myArr);
+//console.log( newArr);
+
+////////>>>>>>>Slice and splice<<<<<<<<<<///////////
+const myArr = [0 , 1 , 2 , 3 , 4 , 5]
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+/*/////////************************arrays 2nd video ////////////////*/
+
+
+
+
