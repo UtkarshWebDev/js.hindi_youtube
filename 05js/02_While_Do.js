bf261353a850cb263636662c97c48bcbbd5493ea
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-13 13:10:42 +0000

    do

---
+++ 05js/02_While_Do.js
@@ -0,0 +1,22 @@
+
+/*let index = 0
+while (index <= 10){
+console.log(`value of index ${index}`);
+index = index + 5
+}
+*/
+
+
+//////////////////////////////           2          ////////////////////////////
+
+let myArray = ["superman","batman", "flash"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+
+    arr = arr + 1
+}
+
+
+
+/////////////////////////////      DO while /////////////// 
