d5f9c9d3c2391580693fe78b98b40dc8a09e43ec
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-14 06:00:15 +0000

    while and do while loops

--- 05js/02_While_Do.js
+++ 05js/02_While_Do.js
@@ -12,9 +12,17 @@ index = index + 5
 let myArray = ["superman","batman", "flash"]
 let arr = 0
 while (arr < myArray.length) {
-    console.log(`value is ${myArray[arr]}`);
+    //console.log(`value is ${myArray[arr]}`);
 
     arr = arr + 1
 }
 
 /////////////////////////////      DO while /////////////// 
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+score++
+
+} while (score <= 10);
