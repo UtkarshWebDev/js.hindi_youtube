9742a429d86ce4dd2d56753c20fd9f74e37f3bb8
UtkarshWebDev <144382533+UtkarshWebDev@users.noreply.github.com>
2023-10-14 07:35:21 +0000

    For In loops

---
+++ 05js/04_For_In.js
@@ -0,0 +1,44 @@
+
+const myObject = {
+    Js :"javascrit",
+    Cpp : "C++",
+    rb : "Ruby",
+    Swift : "Swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(key);
+    //console.log(myObject[key]);
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+
+
+///////////////////////      for array    ////////////////////////
+
+
+const programming = ["js" , "rb" , "py", "java", "cpp" ]
+
+for (const key in programming ) {
+  //console.log(key); 
+  //console.log(programming[key]);
+}
+
+
+///////////////////////////////    maps               /////////////////
+
+
+
+/*const  map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('Fr' , "France")
+map.set('IN', "India")
+
+
+for (const key in map) {
+    console.log(key);   
+}
+
+*/
